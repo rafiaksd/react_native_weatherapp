aec0d541c766bc8d6351631a9e32771ff8742de9
An0nym0usShad0w <76418474+rafiaksd@users.noreply.github.com>
2025-08-25 22:18:17 +0600

    changed message

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View>
-      <Text className="text-4xl font-[#ffccff]">HomeScreen</Text>
+      <Text className="text-4xl font-[#ffccff]">HomeScreen i mean ig a change</Text>
     </View>
   );
 }

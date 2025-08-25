d447181921604b5b392b8159e77dda29c53bfc8b
An0nym0usShad0w <76418474+rafiaksd@users.noreply.github.com>
2025-08-25 22:10:58 +0600

    v0 barebone setup react native tailwind

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text className="text-4xl font-[#ffccff]">HomeScreen</Text>
+    </View>
+  );
+}

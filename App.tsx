11c71ce8ef592482b1199dff3b362b96a4382b07
An0nym0usShad0w <76418474+rafiaksd@users.noreply.github.com>
2025-08-25 22:34:04 +0600

    v0.1 implemented tailwind

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import "./global.css";
 import AppNavigation from "./navigation/appNavigation";
 
 export default function App(){
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import "../global.css";
 
 export default function HomeScreen() {
   return (
     <View>
-      <Text className="text-4xl font-[#ffccff]">HomeScreen i mean ig a change</Text>
+      <Text className="text-4xl font-[#ffccff]">HomeScreen i mean ig a changea</Text>
     </View>
   );
 }

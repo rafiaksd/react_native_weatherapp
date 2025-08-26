b816a4f7265265bb3a6630fa537fb86fd30322ef
An0nym0usShad0w <76418474+rafiaksd@users.noreply.github.com>
2025-08-26 21:17:07 +0600

    search button, location, forecast, icons, other stats

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
+import { StatusBar } from 'react-native';
 import "./global.css";
 import AppNavigation from "./navigation/appNavigation";
 
 export default function App(){
      return (
+          <>
+          <StatusBar hidden={true} />
           <AppNavigation />
+          </>
      )
 }
